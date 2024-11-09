962d76355c8e53c77b1d7c126cc040b4586c0b19
4esterea <4esterea@gmail.com>
2024-11-09 19:31:37 +0100

    feat: initial symTable structure

--- src/symTable/symTable.c
+++ src/symTable/symTable.c
@@ -1,1 +1,23 @@
 #include "symTable.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void frame_init(bst_node_t **tree) {
+    *tree = NULL;
+}
+
+void symTable_init(symtable_t *table) {
+    table->global_frame = (bst_t *)malloc(sizeof(bst_t));
+    frame_init(&(table->global_frame->root));
+    table->global_frame->lower_frame = NULL;
+    table->current_frame = table->global_frame;
+}
+
+void add_frame(symtable_t *table) {
+    bst_t *new_frame = (bst_t *)malloc(sizeof(bst_t));
+    frame_init(&(new_frame->root));
+    new_frame->lower_frame = table->current_frame;
+    table->current_frame = new_frame;
+}
--- src/symTable/symTable.h
+++ src/symTable/symTable.h
@@ -1,4 +1,51 @@
 #ifndef SYMTABLE_H
 #define SYMTABLE_H
 
-#endif //SYMTABLE_H
+#include <stdbool.h>
+#include "../lexer/scanner.h"
+
+typedef enum {
+    ITEM_VARIABLE,
+    ITEM_FUNCTION
+} item_type_t;
+
+typedef struct {
+    TokenType type;
+    char *id;
+    char *value;
+} variable_data_t;
+
+typedef struct function_data_t {
+    TokenType return_type;
+    char *id;
+    variable_data_t *arguments;
+} function_data_t;
+
+typedef union {
+    variable_data_t variable;
+    struct function_data_t *function;
+} item_data_t;
+
+typedef struct bst_node {
+    char *key;
+    item_type_t type;
+    item_data_t data;
+    struct bst_node *left;
+    struct bst_node *right;
+} bst_node_t;
+
+typedef struct {
+    bst_node_t *root;
+    struct bst_t *lower_frame;
+} bst_t;
+
+typedef struct {
+    bst_t *current_frame;
+    bst_t *global_frame;
+} symtable_t;
+
+void frame_init(bst_node_t **tree);
+void symTable_init(symtable_t *table);
+void add_frame(symtable_t *table);
+
+#endif // SYMTABLE_H
